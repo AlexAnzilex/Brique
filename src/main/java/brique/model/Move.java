ca59d56f4d8c6142b09fcd8e0524e990e4509911
Nicola Mande <nicomande03@gmail.com>
2025-06-20 09:57:17 +0200

    Move: create variable of the pie move .

--- src/main/java/brique/model/Move.java
+++ src/main/java/brique/model/Move.java
@@ -4,11 +4,13 @@ public class Move {
     private final int row;
     private final int col;
     private final Player player;
+    private final boolean pie;
 
-    public Move(int row, int col, Player player) {
+    public Move(int row, int col, Player player, boolean pie) {
         this.row = row;
         this.col = col;
         this.player = player;
+        this.pie = pie;
     }
     public int getRow() {
         return row;
@@ -19,4 +21,5 @@ public class Move {
     public Player getPlayer() {
         return player;
     }
+    public boolean isPie() {return pie;}
 }
